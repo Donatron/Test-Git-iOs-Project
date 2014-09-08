0b16f8c58e065a0639414d58bda6e8774d01c168
Don Macarthur <Donatron@Don-Macarthurs-MacBook-Pro-2.local>
2014-09-08 12:18:11 +1000

    added a test class

---
+++ Git Test/DRMTest.h
@@ -0,0 +1,13 @@
+//
+//  DRMTest.h
+//  Git Test
+//
+//  Created by Don Macarthur on 8/09/2014.
+//  Copyright (c) 2014 Donatron Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DRMTest : NSObject
+
+@end
---
+++ Git Test/DRMTest.m
@@ -0,0 +1,13 @@
+//
+//  DRMTest.m
+//  Git Test
+//
+//  Created by Don Macarthur on 8/09/2014.
+//  Copyright (c) 2014 Donatron Productions. All rights reserved.
+//
+
+#import "DRMTest.h"
+
+@implementation DRMTest
+
+@end
